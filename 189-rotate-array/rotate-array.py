408fcae3906fb4752b5afe0b14b5ca2a004225a3
nancysahuofficial21-sudo <nancysahuofficial21@gmail.com>
2026-07-03 21:03:23 +0530

    Time: 39 ms (9.92%) | Memory: 26.7 MB (25.99%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -1,18 +1,27 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-
-        """
-        n=len(nums)
-        k=k%n
-        def reverse(start,end):
-            while start<end:
-                nums[start], nums[end]= nums[end], nums[start]
-                start += 1  # Moves the left pointer RIGHT (increases index)
-                end -= 1    # Moves the right pointer LEFT (decreases index)
-        reverse(0,n-1)
-        reverse(0,k-1)
-        reverse(k,n-1)
+        #op=[0]*len(nums)
+        i=0
+        stagedElement=None
+        visitedCount=0
+        while(visitedCount!=len(nums)):
+            start=i
+            while(True):
+                nextElement=(i+k)%(len(nums))
+                if(stagedElement==None):
+                    x=nums[nextElement]
+                    nums[nextElement]=nums[i]
+                    stagedElement=x
+                else:
+                    x=nums[nextElement]
+                    nums[nextElement]=stagedElement
+                    stagedElement=x
+                visitedCount=visitedCount+1
+                i=nextElement
+                print(stagedElement)
+                if(i==start):
+                    i=i+1
+                    stagedElement=None
+                    break
             
             
