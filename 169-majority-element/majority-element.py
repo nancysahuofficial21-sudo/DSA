2e6f01f7d31279723d6af1db08ec9f10eeb5db97
nancysahuofficial21-sudo <nancysahuofficial21@gmail.com>
2026-07-03 12:01:19 +0530

    Time: 0 ms (100.00%) | Memory: 21.1 MB (86.55%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,9 +1,5 @@
-from collections import defaultdict
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        seen=defaultdict(int)
-        for num in nums:
-            seen[num]+=1
-        for key in seen:
-            if seen[key]>len(nums)//2:
-                return key
+        nums.sort()
+        return nums[len(nums) // 2]
+        
