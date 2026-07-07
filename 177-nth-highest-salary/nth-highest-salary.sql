2d46d2b9325241bb042d6cdbb6b1eb12f56e3318
nancysahuofficial21-sudo <nancysahuofficial21@gmail.com>
2026-07-08 00:19:51 +0530

    Time: 424 ms (95.62%) | Memory: 0B (100.00%) - LeetSync

--- 177-nth-highest-salary/nth-highest-salary.sql
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -4,10 +4,10 @@ BEGIN
       # Write your MySQL query statement below.
       SELECT Salary as getNthHigehstSalary
       FROM (
-            SELECT Salary, DENSE_RANK() OVER ( ORDER BY Salary DESC) rnk
+            SELECT DISTINCT Salary, DENSE_RANK() OVER ( ORDER BY Salary DESC) rnk
             FROM Employee
       ) t
       WHERE rnk = n
-      LIMIT 1
+      
   );
 END
