bc0948e25097406606884be5bd28e1154303a67e
nancysahuofficial21-sudo <nancysahuofficial21@gmail.com>
2026-08-07 00:01:14 +0530

    Time: 211 ms (13.41%) | Memory: 19.9 MB (10.03%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -4,13 +4,9 @@ class Solution:
         seen=set()
         ml=0
         for i in range(len(s)):
-            if s[i] not in seen:
-                seen.add(s[i])
-                ml=max(ml,(i-l)+1)
-            else:
-                while s[i] in seen:
-                    seen.remove(s[l])
-                    l+=1
-                seen.add(s[i]) 
-                ml=max(ml,i-l+1)
+            while s[i] in seen:
+                seen.remove(s[l])
+                l+=1
+            seen.add(s[i]) 
+            ml=max(ml,i-l+1)
         return ml     
