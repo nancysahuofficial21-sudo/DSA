2ca2e0156bef66691e82ba996767e6b5d8ed7eb1
nancysahuofficial21-sudo <nancysahuofficial21@gmail.com>
2026-08-11 20:49:05 +0530

    Time: 856 ms (20.06%) | Memory: 22.4 MB (17.72%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,35 +1,26 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
-        res = []
         nums.sort()
-
+        res=[]
         for i in range(len(nums)):
-            # If the current number is positive, we can never reach a sum of 0
-            if nums[i] > 0:
-                break
-            
-            # Skip duplicates for the first number
-            if i > 0 and nums[i] == nums[i - 1]:
+            if i>0 and nums[i]==nums[i-1]:
                 continue
-            
-            # Use two pointers for the remaining part of the array
-            left, right = i + 1, len(nums) - 1
-            while left < right:
-                three_sum = nums[i] + nums[left] + nums[right]
-                
-                if three_sum < 0:
-                    left += 1  # We need a larger sum
-                elif three_sum > 0:
-                    right -= 1 # We need a smaller sum
-                else:
-                    res.append([nums[i], nums[left], nums[right]])
-                    left += 1
-                    right -= 1
+            j=i+1
+            k=len(nums)-1
+            while j<k:
+                if nums[j]+nums[k]+nums[i]==0:
+                    res.append([nums[i],nums[j],nums[k]])
+                    while j<k and nums[j]==nums[j+1]:
+                        j+=1
+                    while j<k and nums[k]==nums[k-1]:
+                        k-=1
+                    j+=1
+                    k-=1
                     
-                    # Skip duplicates for the left pointer
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
-                        
+                elif nums[j]+nums[k]+nums[i]>0:
+                    k-=1
+                else:
+                    j+=1
+                    
         return res
-
         
