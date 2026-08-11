0c9d2c2f8557fdb05f7a6afb1edf1850c0434805
nancysahuofficial21-sudo <nancysahuofficial21@gmail.com>
2026-08-11 21:00:59 +0530

    Time: 660 ms (34.38%) | Memory: 22.3 MB (31.72%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -18,8 +18,6 @@ class Solution:
                     res.append([nums[i],nums[j],nums[k]])
                     while j<k and nums[j]==nums[j+1]:
                         j+=1
-                    while j<k and nums[k]==nums[k-1]:
-                        k-=1
                     j+=1
                     k-=1
                     
