f40d01a1ce86c06a878f14036f79040116e2f446
nancysahuofficial21-sudo <nancysahuofficial21@gmail.com>
2026-08-16 03:23:15 +0530

    Time: 491 ms (5.13%) | Memory: 26 MB (13.40%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -0,0 +1,16 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        dic={}
+        left=0
+        maxlen=-float('inf')
+        for ri in range(len(fruits)):
+            dic[fruits[ri]]= dic.get(fruits[ri],0)+1
+            while len(dic)>2:
+                dic[fruits[left]]-=1
+                if dic[fruits[left]]==0:
+                    del dic[fruits[left]]
+                left+=1
+            maxlen=max(maxlen, ri-left+1)
+            print(dic)
+        return len(fruits) if maxlen==-float('inf') else maxlen
+        
