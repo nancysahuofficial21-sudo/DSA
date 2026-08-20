5ba2c650923c591538f2747f40efa74eadde3355
nancysahuofficial21-sudo <nancysahuofficial21@gmail.com>
2026-08-21 00:59:22 +0530

    Time: 11 ms (30.35%) | Memory: 20.7 MB (6.04%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -8,14 +8,10 @@ class Solution:
             sumleft.append(sumleft[i]+nums[i])
         for k in range(1,len(nums)):
             sumri.append(sumri[k-1]-nums[k])
-
-        print(sumleft)
-        print(sumri)
         j=0
         while j<len(sumleft) and j<len(sumri):
             if sumleft[j]==sumri[j]:
                 return j
             j+=1
-       
         return -1
         
