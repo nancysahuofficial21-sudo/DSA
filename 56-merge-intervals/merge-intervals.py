39111de7a57d0c8f789d8b0568e64c1d88fd1f5f
nancysahuofficial21-sudo <nancysahuofficial21@gmail.com>
2026-09-02 00:20:35 +0530

    Time: 5 ms (81.30%) | Memory: 22.4 MB (71.87%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.py
@@ -0,0 +1,16 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if len(intervals)<=1:
+            return intervals
+        intervals.sort()   
+        res=[intervals[0]]
+        for i in range(1,len(intervals)):
+            if res[-1][1]>intervals[i][0] and res[-1][1]>intervals[i][1]:
+                pass
+            elif res[-1][1]>=intervals[i][0]:
+                res[-1][1]=intervals[i][1]
+           
+                
+            else:
+                res.append(intervals[i])
+        return res
