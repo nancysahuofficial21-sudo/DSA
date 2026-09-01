ff1a95e0036727d7ee0b5978cc50b685994e35df
nancysahuofficial21-sudo <nancysahuofficial21@gmail.com>
2026-09-02 01:49:08 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (45.99%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -2,11 +2,7 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s)!=len(t):
             return False
-        d1={}
-        d2={}
-        for i in range(len(s)):
-            d1[s[i]]=d1.get(s[i],0)+1
-            d2[t[i]]=d2.get(t[i],0)+1
-        if d1==d2:
-            return True
-        return False
+        for ch in set(s):
+            if s.count(ch) != t.count(ch):
+                return False
+        return True
